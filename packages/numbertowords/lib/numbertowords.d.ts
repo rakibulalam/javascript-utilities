7c9a8231abeed3feb6fc3d1b0dcf830f37a8e325
Md. Rakibul Alam <rakibul.alam@circlefintech.com>
2020-12-05 00:29:12 +0600

    feat:numbertowords

--- packages/numbertowords/lib/numbertowords.d.ts
+++ packages/numbertowords/lib/numbertowords.d.ts
@@ -1,1 +1,9 @@
-export declare const NumberToWords: (value: number) => void;
+export declare type numberToWordOptionTypes = {
+    LESS_THAN_TWENTY: string[];
+    TENTHS_LESS_THAN_HUNDRED: string[];
+    QUADRILLION_TO_HUNDRED: string[];
+    OVERFLOW_MESSAGE: string;
+    IS_SHOW_ONLY: boolean;
+};
+declare const _default: (value: number, options?: numberToWordOptionTypes) => string;
+export default _default;
